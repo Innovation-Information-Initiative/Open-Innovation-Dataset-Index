0509200010aefb68a9e0c3089f45e8550b505bf7
Agnes Cameron <agnesfcameron@gmail.com>
2021-07-13 13:12:27 +0100

    setting up python script to run separately

--- entrypoint.py
+++ entrypoint.py
@@ -4,9 +4,9 @@ import os
 from gsheets_to_csv import load_sheets_into_csv
 
 if __name__ == "__main__":
-    creds = json.loads(os.environ.get("creds", "{}"))
-    sheets = json.loads(os.environ.get("sheets", "{}"))
-    output_dir = os.environ.get("tempdir")
+    creds = json.loads(os.environ.get("INPUT_CREDS", "{}"))
+    sheets = json.loads(os.environ.get("INPUT_SHEETS", "{}"))
+    output_dir = os.environ.get("INPUT_TEMPDIR")
 
     outputs = load_sheets_into_csv(
         sheets=sheets,
