f21dddac4c01fc1c31d05fb31e4d28cf72d2cbc0
Agnes Cameron <agnesfcameron@gmail.com>
2021-07-13 12:57:24 +0100

    testing moving action locally

---
+++ entrypoint.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from gsheets_to_csv import load_sheets_into_csv
+
+if __name__ == "__main__":
+    creds = json.loads(os.environ.get("creds", "{}"))
+    sheets = json.loads(os.environ.get("sheets", "{}"))
+    output_dir = os.environ.get("tempdir")
+
+    outputs = load_sheets_into_csv(
+        sheets=sheets,
+        output_dir=output_dir,
+        creds=creds,
+    )
+
+    json_results = os.environ.get("INPUT_OUTPUT_JSON")
+    print("JSON_RESULTS", json_results)
+    if json_results in {"true", "True"}:
+        results = json.dumps(outputs)
+    else:
+        results = " ".join(outputs)
+
+    print(f"::set-output name=results::{results}")
---
+++ gsheets_to_csv/__init__.py
@@ -0,0 +1,50 @@
+import csv
+import logging
+import os
+
+import gspread  # type: ignore
+
+logger = logging.getLogger("gsheets-to-csv")
+logger.setLevel(logging.INFO)
+
+
+def write_worksheet_to_csv(data, file):
+    with open(file, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+        writer.writerows(data)
+
+
+def load_sheets_into_csv(sheets, output_dir, creds):
+    scope = ["https://spreadsheets.google.com/feeds"]
+    gc = gspread.service_account_from_dict(creds, scope)
+
+    os.makedirs(output_dir, exist_ok=True)
+    outputs = []
+
+    for i, sh in enumerate(sheets):
+        logger.info(f"downloading sheet: {sh}")
+        print(f"downloading sheet: {sh}")
+        if "id" not in sh:
+            logger.warning("Id required to lookup sheet")
+            continue
+        sheet = gc.open_by_key(key=sh["id"])
+
+        try:
+            if "title" in sh:
+                ws = sheet.worksheet(title=sh["title"])
+            else:
+                logger.info("Sheet title not specified; selecting first")
+                ws = sheet.worksheets()[0]
+        except gspread.exceptions.APIError:
+            raise Exception(f"failed to download sheet {sh}")
+
+        if "range" in sh:
+            data = ws.get(sh["range"])
+        else:
+            data = ws.get()
+
+        filename = os.path.join(output_dir, f"tmp{i}.csv")
+        write_worksheet_to_csv(data, filename)
+        outputs.append(filename)
+        logger.info(f"sheet written to {filename}")
+    return outputs
