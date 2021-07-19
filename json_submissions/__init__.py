db6601dfc2ccf0e7662c07d772896ade63bd5070
Agnes Cameron <agnesfcameron@gmail.com>
2021-07-19 15:47:52 +0100

    corrected loop

--- json_submissions/__init__.py
+++ json_submissions/__init__.py
@@ -6,16 +6,18 @@ import os
 import gspread
 
 def parse_and_submit(sheet_id, output_dir, creds):
+	with open(os.environ.get("INPUT_FILE"), 'r') as json_file:
+		data = json.load(json_file)
+		print(data)
+
+	row=[]
+	for val in data.values():
+		print(val)
+		row.append(val)
+
 	scope = ["https://spreadsheets.google.com/feeds"]
 	gc = gspread.service_account_from_dict(creds, scope)
 
-	with open(os.environ.get("INPUT_FILE"), 'r') as json_file:
-		data = json.load(json_file)
-
-	row=[]
-	for item in data:
-		for attribute, value in item:
-			row.append(value)
 
 	#append to google sheet
 	sheet = gc.open_by_key(key=sheet_id)
