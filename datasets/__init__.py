b64374f30d6400783f758b7ceab113b2144ea4f4
Agnes Cameron <agnesfcameron@gmail.com>
2021-08-10 13:51:58 +0100

    crosswalks mediawiki to sheet schema

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,12 +1,12 @@
 import json
 import requests
 import csv
-import logging
-import os
+import sys
 import re
 import gspread
 import frontmatter
 import pandas as pd
+from datetime import datetime
 from urllib.parse import urlparse
 
 def parse_and_submit(new_file, sheet_id, output_dir, creds):
@@ -29,17 +29,45 @@ def parse_and_submit(new_file, sheet_id, output_dir, creds):
 	#get citation metadata associated with file
 	dataset = frontmatter.load(new_file)
 	parsed_url = urlparse(dataset['url'])
-	req = 'https://en.wikipedia.org/api/rest_v1/data/citation/zotero/' + parsed_url.netloc + parsed_url.path + parsed_url.params
-	req = re.sub(r'\/$', '', req)
+	wiki_req = 'https://en.wikipedia.org/api/rest_v1/data/citation/mediawiki/' + parsed_url.netloc + parsed_url.path + parsed_url.params
+	wiki_req = re.sub(r'\/$', '', wiki_req)
+	wiki_res = requests.get(wiki_req)
+	result = wiki_res.json()[0]
 
-	res = requests.get(req)
-	json = res.json()
-	print(json)
+	bib_req = 'https://en.wikipedia.org/api/rest_v1/data/citation/bibtex/' + parsed_url.netloc + parsed_url.path + parsed_url.params
+	bib_req = re.sub(r'\/$', '', bib_req)
+	citation = requests.get(bib_req)
 
-	if len(json.items) > 0:
-		print('length is greater than 0')
-	else:
-		print('length is 0')
+	print(list(map(lambda item: item["tag"].split(), result["tags"])))
+
+	# try:
+		#crosswalk zotero to sheet schema
+	metadata = {
+		"Title": result["title"],
+		"Record Creation Timestamp": datetime.now(),
+		"URL": result["url"],
+		"DOI?": result["extra"] if "DOI" in result["extra"] else '',
+		"I3 member author?": "",
+		"Would you like to add additional metadata?": '',
+		"Description": result["abstractNote"],
+		"Terms of use": "",
+		"Timeframe": "",
+		"Documentation": "",
+		"Performance/error metrics": "",
+		"Citation": citation,
+		"Open-source code": "",
+		"Versioning": "",
+		"API or Bulk downloads": "",
+		"Keywords associated with this dataset": list(map(lambda item: item["tag"], result["tags"])),
+		"Datasets and publications using this dataset": ""
+	}
+
+	new_df = pd.json_normalize(metadata)
+	df = df.append(new_df, verify_integrity=True)
+	print(df)
+	# except:
+	# 	e = sys.exc_info()[0]
+	# 	print('could not fetch metadata', e)
 
 	#if nothing returned, just append title and url (in future append message to PR)
 
