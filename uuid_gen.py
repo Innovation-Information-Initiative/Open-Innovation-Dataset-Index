9562850a4a96a6cf9d70d9e0b7f1410794577aea
Agnes Cameron <agnesfcameron@gmail.com>
2021-10-11 17:44:23 +0100

    adding collection merging tool

---
+++ uuid_gen.py
@@ -0,0 +1,14 @@
+import io
+import uuid
+import frontmatter
+
+if __name__ == "__main__":
+	new_file = sys.argv[1]
+	new_uuid = uuid.uuid4()
+	new_file['uuid'] = str(new_uuid)
+
+	# write out additions to metadata
+	f = io.open(new_file, 'w', encoding='utf8')
+	frontmatter.dump(new_file, f)
+	f.close()
+
