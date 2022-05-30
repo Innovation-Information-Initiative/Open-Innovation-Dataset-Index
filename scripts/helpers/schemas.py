cd4bc79b61f29800188ace12f18a0379d3a7675b
Agnes Cameron <agnesfcameron@gmail.com>
2022-05-30 11:33:09 +0100

    added oai to schemas

--- scripts/helpers/schemas.py
+++ scripts/helpers/schemas.py
@@ -4,7 +4,10 @@ import google
 from google.cloud import bigquery
 from furl import furl
 from datetime import datetime
+import pandas as pd
+import os
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'keys/sheets_key.json'
 client = bigquery.Client()
 
 def check_bigquery(data):
@@ -13,6 +16,29 @@ def check_bigquery(data):
 		if 'bigquery' in row and row['bigquery'].strip() != '':
 			bq_fields.append({'uuid': row['uuid'], 'bq_url': row['bigquery']})
 	return bq_fields
+
+
+def match_salient_fields(salient_fields_path, fields_list):
+	salient_fields = pd.read_csv(salient_fields_path, header=0)
+	suggestions = []
+	for field_name in salient_fields['Name'].values:
+		possibilities = []
+		for field in fields_list:
+			ratio = fuzz.ratio(field, field_name)
+			if ratio > 60:
+				possibilities.append({
+					'field': field,
+					'salient_field': field_name,
+					'score': ratio
+					})
+		if len(possibilities) > 0:
+			top_suggestion = sorted(possibilities, key=lambda poss: poss['score'])[0]
+			suggestions.append(top_suggestion)
+	if len(suggestions) > 0:
+		print(row['title'], suggestions)
+
+	return suggestions
+
 
 def get_schema_fields(bq_project):
 	field_set = set()
@@ -72,7 +98,7 @@ def get_bq_datasets(bq_fields):
 
 	return bq_fields
 
-def get_schemas(data):
+def get_schemas(data, salient_fields_path):
 	update_metadata = False
 	bq_fields = check_bigquery(data)
 	bq_projects = get_bq_datasets(bq_fields)
@@ -85,17 +111,31 @@ def get_schemas(data):
 		if project is not None:
 			# do we need to update the sheet
 			# convert row back to list -> sets and then compare
-			row_schema_list = row['schema_fields'].split(', ')
-			if 'schema_fields' in row and set(project['schema']) != set(row_schema_list) and row_schema_list != ['']:
-				print("sets don't match")
-				update_metadata = True
-				row['last_edit'] =  datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-				row['schema_fields'] = ', '.join(project['schema'])
-			elif 'schema_fields' not in row:
+			if 'schema_fields' in row:
+				row_schema_list = row['schema_fields'].split(', ')
+				if set(project['schema']) != set(row_schema_list) and row_schema_list != ['']:
+					print("sets don't match")
+					update_metadata = True
+					row['schema_fields'] = ', '.join(project['schema'])
+			else:
 				print("field doesn't exist")
 				update_metadata = True
-				row['last_edit'] =  datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 				row['schema_fields'] = ', '.join(project['schema'])
+
+			# match salient fields
+			salient_fields = match_salient_fields(salient_fields_path, project['schema'])
+			if 'salient_fields' in row:
+				row_salient_fields = row['salient_fields'].split(', ')
+				if set(salient_fields) != set(row_salient_fields):
+					update_metadata = True
+					row['schema_fields'] = ', '.join(salient_fields)
+			else:
+				update_metadata = True
+				row['salient_fields'] = ', '.join(salient_fields)
+
+			if update_metadata == True:
+				row['last_edit'] =  datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
 			print('added fields to', row['title'])
 
 	return data, update_metadata
