f35ebbf48ed99d3cfc6a30d77b83ab211a37b0a0
Agnes Cameron <agnesfcameron@gmail.com>
2023-12-01 17:41:05 +0000

    added guides text

--- app/src/pages/guides.js
+++ app/src/pages/guides.js
@@ -28,9 +28,9 @@ const GuidesPage = () => {
     <Layout>
         <div>
         <h1>Guides</h1>
-        <p>
-        
-        </p>
+          <p>Guides are curated resources written by the community that address a specific theme in innovation research, and provide a space for discussing how different tools, datasets and techniques can be used in combination with one another. We also aggregate collections of datasets and tools curated by other platforms (e.g. the Lens Labs tools list, the Google Patents Index of Public Datasets), while others are curated on this site.</p>
+          <p>If you would like to develop a guide -- perhaps you work with multiple datasets in your research, or maybe your department keeps a list of useful tools -- this would be a great thing to share. You are welcome to contribute one yourself via Github (see instructions on the <a href="/about.html">about page</a>), or, if you would like some guidance please <a href="mailto:agnescam@mit.edu">get in touch</a>, we can help.</p>
+          <p>These pages are intentionally free-form, and you are welcome to link to any resources on or off this website (they don't need to be free and open, but it's useful to mention so if not!).</p>
         </div>
         <ul className="indexList">
         {nodes.map(node => (
